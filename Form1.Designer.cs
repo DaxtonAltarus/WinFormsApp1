8182040cce45376581807781253083ae2b4d9a38
DaxtonAltarus <84556668+DaxtonAltarus@users.noreply.github.com>
2021-09-26 23:23:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("3");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("6");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("10");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Meses", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("1");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("2");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("4");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("6");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("10");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Años", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            this.label1 = new System.Windows.Forms.Label();
+            this.rboton1 = new System.Windows.Forms.RadioButton();
+            this.rboton2 = new System.Windows.Forms.RadioButton();
+            this.foto1 = new System.Windows.Forms.PictureBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.foto1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre: ";
+            // 
+            // rboton1
+            // 
+            this.rboton1.AutoSize = true;
+            this.rboton1.Location = new System.Drawing.Point(10, 69);
+            this.rboton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rboton1.Name = "rboton1";
+            this.rboton1.Size = new System.Drawing.Size(69, 19);
+            this.rboton1.TabIndex = 1;
+            this.rboton1.TabStop = true;
+            this.rboton1.Text = "Hombre";
+            this.rboton1.UseVisualStyleBackColor = true;
+            this.rboton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rboton2
+            // 
+            this.rboton2.AutoSize = true;
+            this.rboton2.Location = new System.Drawing.Point(90, 69);
+            this.rboton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rboton2.Name = "rboton2";
+            this.rboton2.Size = new System.Drawing.Size(56, 19);
+            this.rboton2.TabIndex = 2;
+            this.rboton2.TabStop = true;
+            this.rboton2.Text = "Mujer";
+            this.rboton2.UseVisualStyleBackColor = true;
+            // 
+            // foto1
+            // 
+            this.foto1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.foto1.Location = new System.Drawing.Point(299, 9);
+            this.foto1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.foto1.Name = "foto1";
+            this.foto1.Size = new System.Drawing.Size(135, 129);
+            this.foto1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.foto1.TabIndex = 3;
+            this.foto1.TabStop = false;
+            this.foto1.Click += new System.EventHandler(this.foto1_Click);
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(10, 36);
+            this.nombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(228, 23);
+            this.nombre.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 148);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cargar Imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ahuachapán",
+            "Cabañas",
+            "Chalatenango",
+            "Cuscatlán",
+            "La Libertad",
+            "La Paz",
+            "La Unión",
+            "Morazán",
+            "San Miguel",
+            "San Salvador",
+            "San Vicente",
+            "Santa Ana",
+            "Sonsonate",
+            "Usulután"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 126);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(133, 23);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Seleccione un departamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Fecha de nacimiento";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(10, 190);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(219, 23);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeView1.Location = new System.Drawing.Point(36, 246);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "3";
+            treeNode2.Name = "Node2";
+            treeNode2.Text = "6";
+            treeNode3.Name = "Node3";
+            treeNode3.Text = "10";
+            treeNode4.Name = "Node0";
+            treeNode4.Text = "Meses";
+            treeNode5.Name = "Node5";
+            treeNode5.Text = "1";
+            treeNode6.Name = "Node6";
+            treeNode6.Text = "2";
+            treeNode7.Name = "Node7";
+            treeNode7.Text = "4";
+            treeNode8.Name = "Node8";
+            treeNode8.Text = "6";
+            treeNode9.Name = "Node9";
+            treeNode9.Text = "10";
+            treeNode10.Name = "Node4";
+            treeNode10.Text = "Años";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode10});
+            this.treeView1.Size = new System.Drawing.Size(133, 66);
+            this.treeView1.TabIndex = 11;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(49, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Agregar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(276, 501);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 35);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(325, 195);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 19);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Juega tenis";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(302, 270);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(302, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 15);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Referencias acumuladas";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(29, 317);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(393, 133);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Departamento";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Fecha de nacimiento";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 563);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.foto1);
+            this.Controls.Add(this.rboton2);
+            this.Controls.Add(this.rboton1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Formulario de Miembros";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.foto1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rboton1;
+        private System.Windows.Forms.RadioButton rboton2;
+        private System.Windows.Forms.PictureBox foto1;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string imagen = openFileDialog1.FileName;
+                foto1.Image = Image.FromFile(imagen);
+            }
+            else {
+                MessageBox.Show("No se selecciono ninguna fotografia");
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int m = dataGridView1.Rows.Add();
+            dataGridView1.Rows[m].Cells[0].Value = nombre.Text;
+            dataGridView1.Rows[m].Cells[1].Value = comboBox1.SelectedItem;
+            dataGridView1.Rows[m].Cells[2].Value = dateTimePicker1.Value;
+            nombre.Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void foto1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
